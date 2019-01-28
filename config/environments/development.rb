86ab4982aaff0e2934fab55209073faf2102243c
Alex Dunn <dunn.alex@gmail.com>
2019-01-28 09:27:59 -0800

    config: use inline job adapter in dev modes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.active_job.queue_adapter = :inline
 end
